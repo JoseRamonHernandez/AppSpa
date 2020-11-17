5ecd91762e5aa62521bb7642943a4cb06f987293
JoseRamonHernandez <67391682+JoseRamonHernandez@users.noreply.github.com / joseramonhernandezmunoz20@gmail.com>
2020-11-17 15:58:18 -0600

    copia del repositorio de 02-spa
    Para poder publicarla en github pages

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'AppSpa'`, () => {
+  it(`should have as title 'spaApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('AppSpa');
+    expect(app.title).toEqual('spaApp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('AppSpa app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('spaApp app is running!');
   });
 });
